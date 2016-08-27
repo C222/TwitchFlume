5d73f7c38e56e896722eb93bb151c60505bc6287
C222 <compdude222@gmail.com>
2016-08-26 19:09:15 -0700

    Parsing Tags

--- chat_sender/chatsender.go
+++ chat_sender/chatsender.go
@@ -6,6 +6,7 @@ import logger "../logger"
 import "strings"
 import "fmt"
 import "encoding/json"
+import "strconv"
 
 var log = logger.GetLogger()
 
@@ -13,8 +14,22 @@ type ChatSender struct {
   Key string
 }
 
+type Tags struct{
+  Badges string
+  Color string
+  DisplayName string
+  Emotes string
+  Id string
+  Mod bool
+  RoomId int
+  Subscriber bool
+  Turbo bool
+  UserId int
+  UserType string
+}
+
 type ChatLine struct {
-  Tags string
+  Tags
   Channel string
   Message string
 }
@@ -23,10 +38,38 @@ func ParseLine(msg string) *ChatLine {
   var chat_line ChatLine
   split := strings.Split(msg, " PRIVMSG ")
   latter := strings.SplitN(split[1], " :", 2)
+  tags := strings.TrimLeft(strings.SplitN(split[0], " :", 2)[0], "@")
+
+  for _, tag := range strings.Split(tags, ";"){
+    tag_split := strings.SplitN(tag, "=", 2)
+    switch tag_split[0]{
+      case "badges":
+        chat_line.Badges = tag_split[1]
+      case "color":
+        chat_line.Color = tag_split[1]
+      case "display-name":
+        chat_line.DisplayName = tag_split[1]
+      case "emotes":
+        chat_line.Emotes = tag_split[1]
+      case "id":
+        chat_line.Id = tag_split[1]
+      case "mod":
+        chat_line.Mod = (tag_split[1] == "1")
+      case "room-id":
+        chat_line.RoomId, _ = strconv.Atoi(tag_split[1])
+      case "subscriber":
+        chat_line.Subscriber = (tag_split[1] == "1")
+      case "turbo":
+        chat_line.Turbo = (tag_split[1] == "1")
+      case "user-id":
+        chat_line.UserId, _ = strconv.Atoi(tag_split[1])
+      case "user-type":
+        chat_line.UserType = tag_split[1]
+    }
+  }
 
   chat_line.Channel = strings.TrimLeft(latter[0], "#")
-  chat_line.Message = latter[1]
-  chat_line.Tags = strings.SplitN(split[0], " :", 2)[0]
+  chat_line.Message = strings.TrimRight(latter[1], "\n\r")
   return &chat_line
 }
 
