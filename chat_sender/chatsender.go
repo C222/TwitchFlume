cec9e33ede11a30675fb0e517c9d552dd49e3d0f
C222 <c222streams@gmail.com>
2016-08-27 12:47:23 -0700

    Refactored ParseLine, Reformatted

--- chat_sender/chatsender.go
+++ chat_sender/chatsender.go
@@ -11,109 +11,108 @@ import "strconv"
 var log = logger.GetLogger()
 
 type ChatSender struct {
-  Key string
+	Key string
 }
 
-type Tags struct{
-  Badges string
-  Color string
-  DisplayName string
-  Emotes string
-  Id string
-  Mod bool
-  RoomId int
-  Subscriber bool
-  Turbo bool
-  UserId int
-  UserType string
+type Tags struct {
+	Badges      string
+	Color       string
+	DisplayName string
+	Emotes      string
+	Id          string
+	Mod         bool
+	RoomId      int
+	Subscriber  bool
+	Turbo       bool
+	UserId      int
+	UserType    string
 }
 
 type ChatLine struct {
-  Tags
-  Channel string
-  Message string
-  Username string
+	Tags
+	Channel  string
+	Message  string
+	Username string
 }
 
 func handleError(e error, die bool) {
-  if e != nil {
-    if die {
-      log.WithFields(logrus.Fields{
-        "err": e,
-      }).Fatal("WsChat Fatal")
-    } else {
-      log.WithFields(logrus.Fields{
-        "err": e,
-      }).Error("ChatSender Error")
-    }
-  }
+	if e != nil {
+		if die {
+			log.WithFields(logrus.Fields{
+				"err": e,
+			}).Fatal("ChatSender Fatal")
+		} else {
+			log.WithFields(logrus.Fields{
+				"err": e,
+			}).Error("ChatSender Error")
+		}
+	}
 }
 
 func ParseLine(msg string) (*ChatLine, error) {
-  var chat_line ChatLine
+	var chat_line ChatLine
+	split_line := strings.SplitN(msg, " :", 3)
+	if len(split_line) != 3 || !strings.HasSuffix(msg, "\r\n") {
+		return nil, fmt.Errorf("Malformed Chat Line")
+	}
+	tags := split_line[0]
+	for _, tag := range strings.Split(tags, ";") {
+		tag_split := strings.SplitN(tag, "=", 2)
+		switch tag_split[0] {
+		case "badges":
+			chat_line.Badges = tag_split[1]
+		case "color":
+			chat_line.Color = tag_split[1]
+		case "display-name":
+			chat_line.DisplayName = tag_split[1]
+		case "emotes":
+			chat_line.Emotes = tag_split[1]
+		case "id":
+			chat_line.Id = tag_split[1]
+		case "mod":
+			chat_line.Mod = (tag_split[1] == "1")
+		case "room-id":
+			chat_line.RoomId, _ = strconv.Atoi(tag_split[1])
+		case "subscriber":
+			chat_line.Subscriber = (tag_split[1] == "1")
+		case "turbo":
+			chat_line.Turbo = (tag_split[1] == "1")
+		case "user-id":
+			chat_line.UserId, _ = strconv.Atoi(tag_split[1])
+		case "user-type":
+			chat_line.UserType = tag_split[1]
+		}
+	}
 
-  if strings.Count(msg, ":") < 2 {
-    return nil, fmt.Errorf("Malformed Chat Line")
-  }
-  split := strings.Split(msg, " PRIVMSG ")
-  latter := strings.SplitN(split[1], " :", 2)
-  tags := strings.TrimLeft(strings.SplitN(split[0], " :", 2)[0], "@")
+	chat_line.Username = strings.SplitN(split_line[1], "!", 2)[0]
+	chat_line.Channel = strings.SplitN(split_line[1], "#", 2)[1]
 
-  for _, tag := range strings.Split(tags, ";"){
-    tag_split := strings.SplitN(tag, "=", 2)
-    switch tag_split[0]{
-      case "badges":
-        chat_line.Badges = tag_split[1]
-      case "color":
-        chat_line.Color = tag_split[1]
-      case "display-name":
-        chat_line.DisplayName = tag_split[1]
-      case "emotes":
-        chat_line.Emotes = tag_split[1]
-      case "id":
-        chat_line.Id = tag_split[1]
-      case "mod":
-        chat_line.Mod = (tag_split[1] == "1")
-      case "room-id":
-        chat_line.RoomId, _ = strconv.Atoi(tag_split[1])
-      case "subscriber":
-        chat_line.Subscriber = (tag_split[1] == "1")
-      case "turbo":
-        chat_line.Turbo = (tag_split[1] == "1")
-      case "user-id":
-        chat_line.UserId, _ = strconv.Atoi(tag_split[1])
-      case "user-type":
-        chat_line.UserType = tag_split[1]
-    }
-  }
+	chat_line.Message = strings.TrimRight(split_line[2], "\n\r")
 
-  chat_line.Channel = strings.TrimLeft(latter[0], "#")
-  chat_line.Username = strings.SplitN(strings.SplitN(split[0], " :", 2)[1], "!", 2)[0]
-  chat_line.Message = strings.TrimRight(latter[1], "\n\r")
-  return &chat_line, nil
+	return &chat_line, nil
 }
 
 func (self *ChatSender) SendLine(msg string) bool {
-  if strings.Contains(msg, " PRIVMSG "){
-    chat_line, e := ParseLine(msg)
-    if e == nil{
-      log.WithFields(logrus.Fields{
-        "chat": chat_line,
-      }).Debug("Message Parsed")
+	if strings.Contains(msg, " PRIVMSG ") {
+		chat_line, e := ParseLine(msg)
+		if e == nil {
+			log.WithFields(logrus.Fields{
+				"chat": chat_line,
+			}).Debug("Message Parsed")
 
-      uri := fmt.Sprintf("http://logs-01.loggly.com/inputs/%s/tag/%s/", self.Key, chat_line.Channel)
+			uri := fmt.Sprintf("http://logs-01.loggly.com/inputs/%s/tag/%s/", self.Key, chat_line.Channel)
 
-      json_s, e := json.Marshal(chat_line)
-      handleError(e, false)
+			json_s, e := json.Marshal(chat_line)
+			handleError(e, false)
 
-      resp, e := http.Post(uri, "application/json", strings.NewReader(string(json_s)))
-      handleError(e, false)
-      log.WithFields(logrus.Fields{
-        "resp": resp,
-      }).Debug("Message Sent")
+			resp, e := http.Post(uri, "application/json", strings.NewReader(string(json_s)))
+			handleError(e, false)
+			log.WithFields(logrus.Fields{
+				"resp": resp,
+			}).Debug("Message Sent")
 
-      return true
-    }
-  }
-  return false
+			return true
+		}
+	}
+	return false
 }
--- wschat/wschat.go
+++ wschat/wschat.go
@@ -14,136 +14,136 @@ import logger "../logger"
 var log = logger.GetLogger()
 
 type ChatProperties struct {
-  _ int `json:"_id"`
-  _ bool `json:"hide_chat_links"`
-  _ int `json:"chat_delay_duration"`
-  _ bool `json:"devchat"`
-  _ string `json:"game"`
-  _ bool `json:"require_verified_account"`
-  _ bool `json:"subsonly"`
-  _ []string `json:"chat_servers"`
-  WebSocketServers []string `json:"web_socket_servers"`
-  _ float32 `json:"web_socket_pct"`
-  _ float32 `json:"darklaunch_pct"`
-  _ string `json:"block_chat_notification_token"`
-  _ []string `json:"available_chat_notification_tokens"`
-  _ string `json:"sce_title_preset_text_1"`
-  _ string `json:"sce_title_preset_text_2"`
-  _ string `json:"sce_title_preset_text_3"`
-  _ string `json:"sce_title_preset_text_4"`
-  _ string `json:"sce_title_preset_text_5"`
+	_                int      `json:"_id"`
+	_                bool     `json:"hide_chat_links"`
+	_                int      `json:"chat_delay_duration"`
+	_                bool     `json:"devchat"`
+	_                string   `json:"game"`
+	_                bool     `json:"require_verified_account"`
+	_                bool     `json:"subsonly"`
+	_                []string `json:"chat_servers"`
+	WebSocketServers []string `json:"web_socket_servers"`
+	_                float32  `json:"web_socket_pct"`
+	_                float32  `json:"darklaunch_pct"`
+	_                string   `json:"block_chat_notification_token"`
+	_                []string `json:"available_chat_notification_tokens"`
+	_                string   `json:"sce_title_preset_text_1"`
+	_                string   `json:"sce_title_preset_text_2"`
+	_                string   `json:"sce_title_preset_text_3"`
+	_                string   `json:"sce_title_preset_text_4"`
+	_                string   `json:"sce_title_preset_text_5"`
 }
 
 func handleError(e error, die bool) {
-  if e != nil {
-    if die {
-      log.WithFields(logrus.Fields{
-        "err": e,
-      }).Fatal("WsChat Fatal")
-    } else {
-      log.WithFields(logrus.Fields{
-        "err": e,
-      }).Error("WsChat Error")
-    }
-  }
+	if e != nil {
+		if die {
+			log.WithFields(logrus.Fields{
+				"err": e,
+			}).Fatal("WsChat Fatal")
+		} else {
+			log.WithFields(logrus.Fields{
+				"err": e,
+			}).Error("WsChat Error")
+		}
+	}
 }
 
 func getWsUri(channel string) (string, error) {
-  query_uri := fmt.Sprintf("https://api.twitch.tv/api/channels/%s/chat_properties", channel)
+	query_uri := fmt.Sprintf("https://api.twitch.tv/api/channels/%s/chat_properties", channel)
 
-  log.WithFields(logrus.Fields{
-    "query_uri": query_uri,
-  }).Info("Querying")
+	log.WithFields(logrus.Fields{
+		"query_uri": query_uri,
+	}).Info("Querying")
 
-  resp, e := http.Get(query_uri)
+	resp, e := http.Get(query_uri)
 	if e != nil {
 		log.WithFields(logrus.Fields{
 			"err": e,
 		}).Debug("Error quering channel properties")
-    return "", fmt.Errorf("Error quering channel properties")
+		return "", fmt.Errorf("Error quering channel properties")
 	}
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  body, e := ioutil.ReadAll(resp.Body)
+	body, e := ioutil.ReadAll(resp.Body)
 	if e != nil {
 		log.WithFields(logrus.Fields{
 			"err": e,
 		}).Debug("Error reading channel properties")
-    return "", fmt.Errorf("Error reading channel properties")
+		return "", fmt.Errorf("Error reading channel properties")
 	}
 
-  var properties ChatProperties
-  e = json.Unmarshal(body, &properties)
+	var properties ChatProperties
+	e = json.Unmarshal(body, &properties)
 	if e != nil {
 		log.WithFields(logrus.Fields{
 			"err": e,
 		}).Debug("Error parsing channel properties")
-    return "", fmt.Errorf("Error parsing channel properties")
+		return "", fmt.Errorf("Error parsing channel properties")
 	}
 
-  return fmt.Sprintf("ws://%s/", properties.WebSocketServers[0]), nil
+	return fmt.Sprintf("ws://%s/", properties.WebSocketServers[0]), nil
 }
 
 type WsIrc struct {
-  Channel string
-  WS *websocket.Conn
-  OnMessage func(string) bool
+	Channel   string
+	WS        *websocket.Conn
+	OnMessage func(string) bool
 }
 
 func (self *WsIrc) Start() {
-  log.WithFields(logrus.Fields{
-    "channel": self.Channel,
-  }).Info("Starting new connection.")
+	log.WithFields(logrus.Fields{
+		"channel": self.Channel,
+	}).Info("Starting new connection.")
 
-  uri, e := getWsUri(self.Channel)
-  handleError(e, true)
+	uri, e := getWsUri(self.Channel)
+	handleError(e, true)
 
-  log.WithFields(logrus.Fields{
-    "uri": uri,
-  }).Info("Got connection URI.")
+	log.WithFields(logrus.Fields{
+		"uri": uri,
+	}).Info("Got connection URI.")
 
-  origin := "http://localhost/"
-  self.WS, e = websocket.Dial(uri, "", origin)
-  handleError(e, true)
+	origin := "http://localhost/"
+	self.WS, e = websocket.Dial(uri, "", origin)
+	handleError(e, true)
 
-  _, err := self.WS.Write([]byte("CAP REQ :twitch.tv/tags\n"))
-  handleError(err, false)
-  _, err = self.WS.Write([]byte("PASS blah\n"))
-  handleError(err, false)
-  _, err = self.WS.Write([]byte("NICK justinfan47865\n"))
-  handleError(err, false)
+	_, err := self.WS.Write([]byte("CAP REQ :twitch.tv/tags\n"))
+	handleError(err, false)
+	_, err = self.WS.Write([]byte("PASS blah\n"))
+	handleError(err, false)
+	_, err = self.WS.Write([]byte("NICK justinfan47865\n"))
+	handleError(err, false)
 
-  _, err = self.WS.Write([]byte(fmt.Sprintf("JOIN #%s\n", strings.ToLower(self.Channel))))
-  handleError(err, true)
+	_, err = self.WS.Write([]byte(fmt.Sprintf("JOIN #%s\n", strings.ToLower(self.Channel))))
+	handleError(err, true)
 
-  go self.messageListener()
+	go self.messageListener()
 }
 
-func (self *WsIrc) messageListener(){
-  var msg = make([]byte, 65535)
-  var n int
-  var e error
-  var i int = 0
-  var chat_line string
-  for {
-    runtime.KeepAlive(self)
-    n, e = self.WS.Read(msg)
-    handleError(e, false)
-    chat_line = string(msg[:n])
-    if i % 10 == 0 {
-      log.WithFields(logrus.Fields{
-        "i":i,
-        "channel": self.Channel,
-      }).Info("Message Count")
-    }
-    i += 1
-    if strings.HasPrefix(chat_line, "PING") {
-      n, e = self.WS.Write([]byte("PONG\n"))
-      log.Info("PING/PONG")
-    handleError(e, false)
-    } else {
-      go self.OnMessage(chat_line)
-    }
-    runtime.Gosched()
-  }
+func (self *WsIrc) messageListener() {
+	var msg = make([]byte, 65535)
+	var n int
+	var e error
+	var i int = 0
+	var chat_line string
+	for {
+		runtime.KeepAlive(self)
+		n, e = self.WS.Read(msg)
+		handleError(e, false)
+		chat_line = string(msg[:n])
+		if i%10 == 0 {
+			log.WithFields(logrus.Fields{
+				"i":       i,
+				"channel": self.Channel,
+			}).Info("Message Count")
+		}
+		i += 1
+		if strings.HasPrefix(chat_line, "PING") {
+			n, e = self.WS.Write([]byte("PONG\n"))
+			log.Info("PING/PONG")
+			handleError(e, false)
+		} else {
+			go self.OnMessage(chat_line)
+		}
+		runtime.Gosched()
+	}
 }
